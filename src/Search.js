6140206e9f6293cb2636198c63676531bb921a8e
Veselin Tonev <veselinn16@yahoo.com>
2019-08-29 19:29:58 +0300

    Add controlled input :wrench:

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Search = props => (
-  <div className="search">
-    <p>Search</p>
-  </div>
-);
+const Search = props => {
+  const [input, setInput] = useState('');
+  useEffect(() => console.log(input));
+  return (
+    <div className="search">
+      <input
+        type="text"
+        placeholder="Search for movie..."
+        className="search__input"
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <button className="search__button">Go</button>
+    </div>
+  );
+};
 
 export default Search;
