36b5d426b50d708f53e99679973a510b1fcc858a
Veselin Tonev <veselinn16@yahoo.com>
2019-08-30 18:42:32 +0300

    Add state variable for movies :wrench:

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import Search from './Search';
 import Main from './Main';
 
 const App = () => {
+  const [movies, setMovies] = useState(null);
+  const [input, setInput] = useState('');
+
+  const search = async query => {
+    const APIKEY = 'cd7e3b';
+    const baseURL = `http://www.omdbapi.com/?apikey=${APIKEY}&s=${query}`;
+
+    const res = await fetch(baseURL);
+    const data = await res.json();
+
+    setMovies(data.Search);
+  };
+
   return (
     <div className="App">
       <Header />
-      <Search />
-      <Main />
+      <Search input={input} setInput={setInput} search={search} />
+      <Main movies={movies} />
       <Footer />
     </div>
   );
