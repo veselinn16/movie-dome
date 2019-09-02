d9294ec837f69257522edfa516ee4d9205e10ae8
Veselin Tonev <veselinn16@yahoo.com>
2019-09-02 18:52:51 +0300

    Make cards rotate on hover :art:

--- src/Movie.js
+++ src/Movie.js
@@ -4,16 +4,24 @@ const Movie = ({ movie }) => {
   //   useEffect(() => console.log(movie));
   return (
     <div className="movie">
-      <div className="movie__poster__container">
-        <img
-          className="movie__poster"
-          src={movie.Poster ? movie.Poster : '#'}
-          alt={`${movie.Title}'s poster`}
-        />
+      <div className="movie__side movie__side--front">
+        <div className="movie__poster__container">
+          <img
+            className="movie__poster"
+            src={movie.Poster ? movie.Poster : '#'}
+            alt={`${movie.Title}'s poster`}
+          />
+        </div>
+        <div className="movie__data">
+          <h2 className="movie__data--title">{movie.Title}</h2>
+          <h4 className="movie__data--year">{movie.Year}</h4>
+        </div>
       </div>
-      <div className="movie__data">
-        <h2 className="movie__data--title">{movie.Title}</h2>
-        <h4 className="movie__data--year">{movie.Year}</h4>
+      <div className="movie__side movie__side--back">
+        <h3 className="movie__details--text">View details for</h3>
+        <a className="movie__details--link" href="#">
+          {movie.Title}
+        </a>
       </div>
     </div>
   );
