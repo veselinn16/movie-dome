4f251b1f5c3d21b6c0d655419f9d631d2c160d12
Veselin Tonev <veselinn16@yahoo.com>
2019-10-08 20:05:07 +0300

    Add wrapper for search form :wrench:

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Search = ({ input, setInput, search }) => {
   return (
     <div className="search">
-      <input
-        type="text"
-        placeholder="Search for movie..."
-        className="search__input"
-        value={input}
-        onChange={e => setInput(e.target.value)}
-      />
-      <button className="search__button" onClick={() => search(input)}>
-        Go
-      </button>
+      <div className="search__wrapper">
+        <input
+          type="text"
+          placeholder="Search for movie..."
+          className="search__input"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        <button className="search__button" onClick={() => search(input)}>
+          Go
+        </button>
+      </div>
     </div>
   );
 };
